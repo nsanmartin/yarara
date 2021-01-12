c11abb56b5ef4222a63616c34a1c1a739d7be07e
nsm <smnicolas@gmail.com>
2021-01-12 07:47:45 -0300

    Document the test

--- tests/cuadrado.cpp
+++ tests/cuadrado.cpp
@@ -1,7 +1,12 @@
 #include <World.hpp>
-
+#include <iostream>
 
 int main () {
+    std::cout << "Press `s' to start\n"
+              << "Arrows to move\n"
+              << "`r' od `R' to rotate\n"
+              << "`Esc' to clear the triangle\n"
+              << "`q' to quit\n";
     World w{};
     w.Loop();
 }
